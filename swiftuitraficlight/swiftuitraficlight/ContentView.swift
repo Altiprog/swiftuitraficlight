493576745ecb265e99020cece1dae1136a560024
Altiprog <atiunovich@gmail.com>
2023-12-13 18:30:09 +0100

    almost

--- swiftuitraficlight/swiftuitraficlight/ContentView.swift
+++ swiftuitraficlight/swiftuitraficlight/ContentView.swift
@@ -8,17 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var activeCircle = 0
+
     var body: some View {
-        HStack { 
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack {
+            Circle()
+                .foregroundColor(.red)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .frame(width: 100)
+                .shadow(color: .red, radius: 10)
+                .opacity(activeCircle == 1 ? 1.0 : 0.4)
+                
+            Circle()
+                .foregroundColor(.yellow)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .frame(width: 100)
+                .shadow(color: .yellow, radius: 10)
+                .opacity(activeCircle == 2 ? 1.0 : 0.4)
+                
+            Circle()
+                .foregroundColor(.green)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .frame(width: 100)
+                .shadow(color: .green, radius: 10)
+                .opacity(activeCircle == 3 ? 1.0 : 0.4)
+                
+            Spacer()
+
+            Button("START") {
+                withAnimation {
+                    activeCircle = (activeCircle % 3) + 1
+                }
+            }
+            .background(Color.blue)
+            .cornerRadius(5)
+            .foregroundColor(.white)
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
 }
 
+
 #Preview {
-    ContentView()
+    ZStack {
+        Color.black
+            .ignoresSafeArea()
+        ContentView()
+    }
 }
